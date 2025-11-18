8bb698e7822c8139c2f7abbe5dd3c3f30a6b0510
Axel Cabrera <arackcabrera@gmail.com>
2025-11-18 12:52:58 -0600

    componente y cambios

--- megadeliciasapi/megadelicias-frontend/src/app/app.config.ts
+++ megadeliciasapi/megadelicias-frontend/src/app/app.config.ts
@@ -1,29 +1,16 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
-// --- TAREAS DE JARED ---
 import { FormsModule } from '@angular/forms'; // <-- 1. Importa FormsModule
 import { provideHttpClient } from '@angular/common/http'; // <-- 2. Importa provideHttpClient
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    
+
+    // --- AÑADE ESTAS DOS LÍNEAS ---
     provideHttpClient(),             // <-- 3. Añade esto para llamadas API
     importProvidersFrom(FormsModule) // <-- 4. Añade esto para formularios (ngModel)
-  ]
-};
-
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
   ]
 };
---
+++ megadeliciasapi/megadelicias-frontend/src/app/pages/caja-panel/caja-panel.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-caja-panel',
+  imports: [],
+  templateUrl: './caja-panel.html',
+  styleUrl: './caja-panel.css',
+})
+export class CajaPanel {
+
+}
