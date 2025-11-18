ecb85ff08f451780adbddd70edbb1b9a67e0828c
janne <janneli.barahona@ujcv.edu.hn>
2025-11-18 01:45:31 -0600

    Agrega componentes AppLayout y Sidebar

---
+++ megadeliciasapi/megadelicias-frontend/src/app/components/sidebar/sidebar.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router'; // <-- ¡Importa esto!
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [
+    RouterLink,         // <-- ¡Añádelo aquí!
+    RouterLinkActive    // <-- ¡Añádelo aquí!
+  ],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+
+}
