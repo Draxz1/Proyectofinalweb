144f5aaded3847d558a67265dbc68a5c34310785
nelson jared alvarez corea <nelsonjared.hn@gmail.com>
2025-11-23 21:47:33 -0600

    arreglo/basedatos

--- megadeliciasapi/megadeliciasapi/megadeliciasapi/Models/MetodoDePago.cs
+++ megadeliciasapi/megadeliciasapi/megadeliciasapi/Models/MetodoDePago.cs
@@ -4,10 +4,6 @@ namespace megadeliciasapi.Models
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
-        public bool RequiereReferencia { get; set; }
-        public bool EsBancario { get; set; }
-        public bool Activo { get; set; }
-
         public ICollection<Pago> Pagos { get; set; }
         public ICollection<MovimientoCaja> MovimientosCaja { get; set; }
     }
